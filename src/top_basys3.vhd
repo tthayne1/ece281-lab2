0749e589600bea3c8e843b7335cc3d9c1ed3b8e4
tthayne1 <253853281+tthayne1@users.noreply.github.com>
2026-03-03 19:54:20 -0700

    code implementation

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -1,27 +1,28 @@
 --+----------------------------------------------------------------------------
---| 
+--|
 --| COPYRIGHT 2025 United States Air Force Academy All rights reserved.
---| 
---| United States Air Force Academy     __  _______ ___    _________ 
---| Dept of Electrical &               / / / / ___//   |  / ____/   |
---| Computer Engineering              / / / /\__ \/ /| | / /_  / /| |
+--|
+--| United States Air Force Academy     __  _______ ___    _________
+--| Dept of Electrical &
+--| Computer Engineering              / / / / ___//   |  / ____/   |
+--|                                  / / / /\__ \/ /| | / /_  / /| |
 --| 2354 Fairchild Drive Ste 2F6     / /_/ /___/ / ___ |/ __/ / ___ |
 --| USAF Academy, CO 80840           \____//____/_/  |_/_/   /_/  |_|
---| 
+--|
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : top_basys3.vhd
 --| AUTHOR(S)     : Capt Phillip Warner & Capt Brian Yarbrough
 --| CREATED       : 01/22/2018 Last modified 02/18/2025
---| DESCRIPTION   : This file implements the top level module for a BASYS 3 to utilize 
---|					a seven-segment decoder for displaying hex values on seven-segment 
---|					displays (7SD) according to 4-bit inputs provided by switches.
+--| DESCRIPTION   : This file implements the top level module for a BASYS 3 to utilize
+--|                 a seven-segment decoder for displaying hex values on seven-segment
+--|                 displays (7SD) according to 4-bit inputs provided by switches.
 --|
---|					Inputs:  sw (3:0)  --> 4-bit signal to deternmine 7SD value to be diplayed
---|							 btnC	   --> activate 7SD
+--|                 Inputs:  sw (3:0)  --> 4-bit signal to deternmine 7SD value to be diplayed
+--|                          btnC      --> activate 7SD
 --|
---|					Output:  seg (6:0) --> 7-bit signal to activate the individual segments (active low)
---|							 an (3:0)  --> 4-bit signal to control which display turns on (active low)
+--|                 Output:  seg (6:0) --> 7-bit signal to activate the individual segments (active low)
+--|                          an (3:0)  --> 4-bit signal to control which display turns on (active low)
 --|
 --|
 --+----------------------------------------------------------------------------
@@ -60,42 +61,59 @@ library ieee;
 
 
 entity top_basys3 is
-	port(
-		-- 7-segment display segments (cathodes CG ... CA)
-		seg		:	out std_logic_vector(6 downto 0);  -- seg(6) = CG, seg(0) = CA
+    port(
+        -- 7-segment display segments (cathodes CG ... CA)
+        seg     : out std_logic_vector(6 downto 0);  -- seg(6) = CG, seg(0) = CA
 
-		-- 7-segment display active-low enables (anodes)
-		an      :	out std_logic_vector(3 downto 0);
+        -- 7-segment display active-low enables (anodes)
+        an      : out std_logic_vector(3 downto 0);
 
-		-- Switches
-		sw		:	in  std_logic_vector(3 downto 0);
-		
-		-- Buttons
-		btnC	:	in	std_logic
-
-	);
+        -- Switches
+        sw      : in  std_logic_vector(3 downto 0);
+        
+        -- Buttons
+        btnC    : in  std_logic
+    );
 end top_basys3;
 
 architecture top_basys3_arch of top_basys3 is 
-	
+    
   -- declare the component of your top-level design
+  component sevenseg_decoder is
+    port(
+        i_Hex   : in  std_logic_vector(3 downto 0);
+        o_seg_n : out std_logic_vector(6 downto 0)
+    );
+  end component;
 
 
   -- create wire to connect button to 7SD enable (active-low)
-
+  signal w_7SD_EN_n : std_logic;
   
 begin
-	-- PORT MAPS ----------------------------------------
+    -- PORT MAPS ----------------------------------------
 
-	--	Port map: wire your component up to the switches and seven-segment display cathodes
-	-----------------------------------------------------	
-	
-	
-	-- CONCURRENT STATEMENTS ----------------------------
-	
-	-- wire up active-low 7SD anode (active low) to button (active-high)
-	-- display 7SD 0 only when button pushed
-	-- other 7SD are kept off
-	-----------------------------------------------------
-	
+    --  Port map: wire your component up to the switches and seven-segment display cathodes
+    -----------------------------------------------------  
+    U_DEC : sevenseg_decoder
+        port map(
+            i_Hex   => sw,
+            o_seg_n => seg
+        );
+    
+    
+    -- CONCURRENT STATEMENTS ----------------------------
+    
+    -- wire up active-low 7SD anode (active low) to button (active-high)
+    -- display 7SD 0 only when button pushed
+    -- other 7SD are kept off
+    -----------------------------------------------------
+    
+    w_7SD_EN_n <= not btnC;
+
+    an(0) <= w_7SD_EN_n;
+    an(1) <= '1';
+    an(2) <= '1';
+    an(3) <= '1';
+    
 end top_basys3_arch;
